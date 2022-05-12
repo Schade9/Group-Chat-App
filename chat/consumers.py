599f6b6dd18644e97228b32962664329d2271356
Ssemakula Charles Derrick <51287876+Schade9@users.noreply.github.com>
2022-05-12 13:23:04 +0300

    Asynchronous code

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,23 +1,25 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
-from asgiref.sync import async_to_sync
+from .models import ChatModel
+from asgiref.sync import sync_to_async
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         # layer is to be created
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_'+str(self.room_name)
-        async_to_sync(self.channel_layer.group_add)(self.room_group_name,self.channel_name)
-        self.accept()
+        await self.channel_layer.group_add(self.room_group_name,self.channel_name)
+        await self.accept()
 
-    def disconnect(self):
-        async_to_sync(self.channel_layer.group_discard)(self.room_group_name,self.channel_name)
+    async def disconnect(self):
+        await self.channel_layer.group_discard(self.room_group_name,self.channel_name)
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        async_to_sync(self.channel_layer.group_send)(self.room_group_name,{ 'message':message, 'type':'send_back' })
+        await sync_to_async(ChatModel(room_no = self.room_name, message = message).save)()
+        await self.channel_layer.group_send(self.room_group_name,{ 'message':message, 'type':'send_back' })
 
-    def send_back(self, event):
+    async def send_back(self, event):
         message = event['message']
-        self.send(text_data = json.dumps({'message':message}))
+        await self.send(text_data = json.dumps({'message':message}))
