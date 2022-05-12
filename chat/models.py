cb773c5ae58d78a265a1a91f2ee5188208f87a41
Ssemakula Charles Derrick <51287876+Schade9@users.noreply.github.com>
2022-05-12 13:26:28 +0300

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class ChatModel(models.Model):
+    room_no = models.CharField(max_length=128)
+    message = models.TextField()
+
